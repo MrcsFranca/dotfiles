6207acaf89f93b366c2132d79386183b2c63faa7
Marcos França <86272462+MrcsFranca@users.noreply.github.com>
2026-02-10 12:50:09 -0300

    Enable Syncthing and configure Firefox settings

--- NixOS/home.nix
+++ NixOS/home.nix
@@ -3,29 +3,44 @@
 {
     home.username = "marcos";
     home.homeDirectory = "/home/marcos";
-    programs.git.enable = true;
     home.stateVersion = "25.11";
+
+    programs.git.enable = true;
+    services.syncthing.enable = true;
     programs.bash = {
         enable = true;
         shellAliases = {
-            btw = "echo i use nixos, btw";
             nv = "nvim";
         };
     };
     xdg.configFile."qtile" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/home/marcos/nixos-dotfiles/config/qtile/";
-      recursive = true;
+        source = config.lib.file.mkOutOfStoreSymlink "/home/marcos/nixos/config/qtile/";
+        recursive = true;
     };
+    programs.firefox = {
+        enable = true;
+        profiles.marcos = {
+    	    settings = {
+    		"extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+    		"layout.css.prefers-color-scheme.content-override" = 0;
+    		"ui.systemUsesDarkTheme" = 1;
+    		"devtools.theme" = "dark";
+    		"browser.theme.content-theme" = 0;
+    		"browser.theme.toolbar-theme" = 0;
+	    };
+      };
+    };
+
     home.file.".config/nvim".source = ./config/nvim;
     home.file.".config/alacritty".source = ./config/alacritty;
     home.packages = with pkgs; [
-      neovim
-      nil
-      nixpkgs-fmt
-      gcc
-      obsidian
-      zotero
-      ranger
-      lynx
+        neovim
+        nil
+        nixpkgs-fmt
+        gcc
+        obsidian
+        zotero
+        ranger
+        lynx
     ];
 }
